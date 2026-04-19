c51bb42d621db0a27c52aaef9aa415bab12433d2
samarasfernandes <samarasfernandes.dev@gmail.com>
2026-04-19 13:09:06 -0400

    Mudado texto no card contatos

--- src/components/Cardcon.jsx
+++ src/components/Cardcon.jsx
@@ -9,21 +9,21 @@ const Cardcon = () => {
       <div className="card">
         <FaHome className="icon" />
         <h3>VISITE-NOS</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis mollis cuidin arcu non rutrum</p>
+        <p>Trabalhamos com manutenção e conserto de compressores, oferecendo um serviço rápido, confiável e de qualidade.</p>
         <span className="info">Rua Neivo Carlos da Silva, 163<br/> Nova Andradina - MS</span>
       </div>
 
       <div className="card">
         <FaPhone className="icon" />
         <h3>LIGUE PARA NÓS</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis mollis cuidin arcu non rutrum</p>
+        <p>Entre em contato conosco para obter suporte técnico, esclarecer dúvidas ou solicitar um orçamento.</p>
         <span className="info">(67) 99349-8779</span>
       </div>
 
       <div className="card">
         <FaEnvelope className="icon" />
         <h3>CONTATO</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis mollis cuidin arcu non rutrum</p>
+        <p>Fale conosco por e-mail e receba um atendimento rápido e eficiente para suas necessidades.</p>
         <span className="info">novacompressores@terra.com.br</span>
       </div>
 
